d5739d2c06cfd7c91684339ddff088fbb1d390f1
William <william-olofsson@hotmail.com>
2018-01-24 14:43:38 +0100

    Kalendern skapas dynamiskt!

--- Kommunikationsverktyg för informatik/Controllers/CalendarController.cs
+++ Kommunikationsverktyg för informatik/Controllers/CalendarController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Kommunikationsverktyg_för_informatik.ViewModels;
 
 namespace Kommunikationsverktyg_för_informatik.Controllers
 {
@@ -11,7 +12,29 @@ namespace Kommunikationsverktyg_för_informatik.Controllers
         // GET: Calendar
         public ActionResult Calendar()
         {
-            return View();
+            var model = new MonthViewModels
+            {
+                Name = DateTime.Today.ToString("MMMM").ToUpper(),
+                NumberOfDays = DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month),
+                PreviousMonthsNumberOfDays = DateTime.DaysInMonth(DateTime.Now.Year, PreviousMonth(DateTime.Now.Month)),
+                FirstDayOfMonth = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1).Day,
+                CurrentDay = DateTime.Today.Day
+            };
+            return View(model);
+        }
+
+        private int PreviousMonth(int currentMonth)
+        {
+            int month = 0;
+            if (currentMonth == 1)
+            {
+                month = 12;
+            }
+            else
+            {
+                month = currentMonth - 1;
+            }
+            return month;
         }
     }
 }
