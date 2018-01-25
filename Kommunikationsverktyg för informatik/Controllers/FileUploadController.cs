524096c4e8f217b69fa202100159ea25262b50b8
Schmoege <christoffer.isacsson@gmail.com>
2018-01-25 12:47:23 +0100

    Uppladdning fungerar

---
+++ Kommunikationsverktyg för informatik/Controllers/FileUploadController.cs
@@ -0,0 +1,65 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kommunikationsverktyg_för_informatik.Controllers
+{
+
+    
+    public class FileUploadController : Controller
+    {
+        
+        public ActionResult FileUpload()
+        {
+            List<UserFile> files;
+            using (var db = new DataContext())
+            {
+                files = db.UserFiles.ToList();
+            }
+            return View(files);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult UploadFile(HttpPostedFileBase fileToUpload)
+        {
+            try
+            {
+                if (fileToUpload == null)
+                {
+                    RedirectToAction("Index", "Home");
+                }
+                var ownerPost = new Post();
+
+                var newFile = new UserFile();
+                newFile.BlogPost = null; //Ändra till postobjekt senare
+                newFile.FileID = Guid.NewGuid();
+                newFile.FileType = Path.GetExtension(fileToUpload.FileName);
+                newFile.FileName = fileToUpload.FileName;
+                using (var reader = new BinaryReader(fileToUpload.InputStream))
+                {
+                    newFile.Path = reader.ReadBytes(fileToUpload.ContentLength);
+                }
+                using (var dbContext = new DataContext())
+                {
+                    dbContext.UserFiles.Add(newFile);
+                    dbContext.SaveChanges();
+                }
+
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.Write("Hello via Debug!");
+                System.Diagnostics.Debug.Write(e.Message);
+                System.Diagnostics.Debug.Write( "Hello via Debug!" );
+                throw;
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
