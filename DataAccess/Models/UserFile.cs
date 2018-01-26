8b5de9e5b8fe2c43fb8ce33e760ba17a637d187b
Schmoege <christoffer.isacsson@gmail.com>
2018-01-26 10:24:29 +0100

    Upload vid postskapande fungerar

---
+++ DataAccess/Models/UserFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class UserFile
+    {
+        [Key]
+        public Guid FileID { get; set; }
+        public byte[] FileBytes { get; set; }
+        public String FileName { get; set; }
+        public Post BlogPost { get; set; }
+    }
+}
+
--- Kommunikationsverktyg för informatik/Controllers/BlogController.cs
+++ Kommunikationsverktyg för informatik/Controllers/BlogController.cs
@@ -67,6 +67,11 @@ namespace Kommunikationsverktyg_för_informatik.Controllers
                 .Select(x => x.Id).First();
             context.Posts.Add(model.Post);
             context.SaveChanges();
+            if (model.uploadFile != null)
+            {
+                UploadFile(model.uploadFile, model.Post);
+            }
+            
             return RedirectToAction("Index");
         }
 
@@ -88,6 +93,44 @@ namespace Kommunikationsverktyg_för_informatik.Controllers
             return RedirectToAction("Create");
         }
 
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult UploadFile(HttpPostedFileBase fileToUpload, Post ownerPost)
+        {
+            try
+            {
+                
+                var newFile = new UserFile();
+                newFile.BlogPost = ownerPost; //Ändra till postobjekt senare
+                newFile.FileID = Guid.NewGuid();
+                newFile.FileName = fileToUpload.FileName;
+                using (var reader = new System.IO.BinaryReader(fileToUpload.InputStream))
+                {
+                    newFile.FileBytes = reader.ReadBytes(fileToUpload.ContentLength);
+                }
+                using (var dbContext = new DataContext())
+                {
+                    dbContext.UserFiles.Add(newFile);
+                    dbContext.SaveChanges();
+                }
 
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.Write(e.Message);
+                throw;
+            }
+            return RedirectToAction("Index", "Home");
+        }
+        public ActionResult DownloadFile(string downloadFileId)
+        {
+            UserFile fileToDownload;
+            using (var db = new DataContext())
+            {
+                fileToDownload = db.UserFiles.Single(x => x.FileID.Equals(new Guid(downloadFileId)));
+            }
+            return File(fileToDownload.FileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileToDownload.FileName);
+        }
     }
 }
