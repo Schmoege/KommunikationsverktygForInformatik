d2fe1f89ccef601968bd24b5df260ef93c223de6
Jakob <jakob.handfast@gmail.com>
2018-02-09 00:25:26 +0100

    Fixed location

--- DataAccess/Models/Post.cs
+++ DataAccess/Models/Post.cs
@@ -38,6 +38,8 @@ namespace DataAccess.Models
 
         public virtual ICollection<ApplicationUser> AppUsers { get; set; }
 
+        public Location Location { get; set; }
+
         public Post()
         {
             Id = Guid.NewGuid();
---
+++ Kommunikationsverktyg för informatik/Controllers/Api/LocationController.cs
@@ -0,0 +1,29 @@
+﻿using DataAccess.Models;
+using Kommunikationsverktyg_för_informatik.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Kommunikationsverktyg_för_informatik.Controllers.Api
+{
+    public class LocationController : ApiController
+    {
+        [HttpPost]
+        public void Set(LocationViewModel loc)
+        {
+            using (DataContext db = new DataContext())
+            {
+                Location location = new Location()
+                {
+                    Latitude = loc.lat,
+                    Longitude = loc.lng,
+                    PostId = new Guid(loc.PostId)
+                };
+                db.Location.Add(location);
+                db.SaveChanges();
+            }
+        }
+    }
+}
